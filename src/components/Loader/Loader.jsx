2bd0564d1345a8618b2ad7911e0f81906a7f7339
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-15 21:47:19 +0300

    APi fetches and Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,26 @@
+import { DNA } from 'react-loader-spinner';
+
+export const Loader = () => (
+  <DNA
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="dna-loading"
+    wrapperStyle={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      margin: '0 auto',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      zIndex: '9999',
+    }}
+    wrapperClass="dna-wrapper"
+    glassColor="#c0efff"
+    color="#949293"
+  />
+);
