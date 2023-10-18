2fd146f21d21cad7c46e35adabeecb84c3f6419b
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-18 20:26:34 +0300

    working MoviesList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,42 +1,37 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { List, Item } from './MoviesList.styled';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
 
-  return (
-    <>
-      <List>
-        {movies.map(movie => (
-          <Item key={movie.id}>
-            <div
-              to={`/movies/${movie.id}`}
-              state={{ from: location }}
-              cover={movie.poster_path}
-            >
-              {movie.title}
-            </div>
-          </Item>
-        ))}
-      </List>
-    </>
-  );
   // return (
-  //   <List>
-  //     {movies.map(({ id, title, poster_path }) => {
-  //       const imgPoster = searchImages(poster_path);
-  //       console.log(imgPoster);
-  //       return (
-  //         <div state={{ from: location }} to={`/movies/${id}`} key={id}>
-  //           <Item>
-  //             <img src={imgPoster} alt={title} />
-  //             <p>{title}</p>
-  //           </Item>
-  //         </div>
-  //       );
-  //     })}
-  //   </List>
+  //   <>
+  //     <List>
+  //       {movies.map(movie => (
+  //         <Item key={movie.id}>
+  //           <div
+  //             to={`/movies/${movie.id}`}
+  //             state={{ from: location }}
+  //             cover={movie.poster_path}
+  //           >
+  //             {movie.title}
+  //           </div>
+  //         </Item>
+  //       ))}
+  //     </List>
+  //   </>
   // );
+  return (
+    <List>
+      {movies.map(({ id, original_title }) => (
+        <Item key={id}>
+          <Link state={{ from: location }} to={`/movies/${id}`}>
+            {original_title}
+          </Link>
+        </Item>
+      ))}
+    </List>
+  );
 };
 
 export default MoviesList;
