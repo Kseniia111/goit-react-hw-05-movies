9b1064f77ad1b1033d059f6f50ebb808ba96d4df
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-19 20:28:03 +0300

    working movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,55 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-
 import { handleSearch } from 'services/SearchApi';
 import { SearchForm } from 'components/SearchForm/SearchForm';
 import MoviesList from 'components/MoviesList/MoviesList';
 
 const Movies = () => {
-  const [searchResults, setSearchResults] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const movieName = searchParams.get('query') || '';
-  const [loading, setLoading] = useState(false);
-
-  const updateQueryString = query => {
-    const nextParams = query !== '' && { query };
-    setSearchParams(nextParams);
-  };
 
   useEffect(() => {
-    const search = async () => {
+    const currentQuery = searchParams.get('query');
+    if (!currentQuery) return;
+
+    const fetchMovieByQuery = async () => {
       try {
-        setLoading(true);
-        const movies = await handleSearch(movieName);
-        setSearchResults(movies);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+        const movieByQuery = await handleSearch(currentQuery);
+        setMovies(movieByQuery);
+      } catch (e) {
+        console.log(e);
       }
     };
-    search();
-  }, [movieName]);
+    fetchMovieByQuery();
+  }, [searchParams]);
 
   return (
-    <div>
-      <SkeletonTheme baseColor="#dddddd" highlightColor="#a5a5a5">
-        <SearchForm value={movieName} onChange={updateQueryString} />
-        {loading ? (
-          <Skeleton
-            count={15}
-            style={{ height: 30, width: 300, marginTop: 15 }}
-          />
-        ) : searchResults.length === 0 && movieName ? (
-          <h2>🔎 Nothing found</h2>
-        ) : (
-          <MoviesList films={searchResults} />
-        )}
-      </SkeletonTheme>
-    </div>
+    <>
+      <SearchForm setSearchParams={setSearchParams} />
+      {movies.length > 0 && <MoviesList movies={movies} />}
+    </>
   );
 };
 
 export default Movies;
+// const Movies = () => {
+//   const [searchResults, setSearchResults] = useState([]);
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const movieName = searchParams.get('query') || '';
+//   const [loading, setLoading] = useState(false);
+
+//   const updateQueryString = query => {
+//     const nextParams = query !== '' && { query };
+//     setSearchParams(nextParams);
+//   };
+
+//   useEffect(() => {
+//     const search = async () => {
+//       try {
+//         setLoading(true);
+//         const movies = await handleSearch(movieName);
+//         setSearchResults(movies);
+//       } catch (error) {
+//         console.error(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     search();
+//   }, [movieName]);
+
+//   return (
+//     <div>
+//       <SkeletonTheme baseColor="#dddddd" highlightColor="#a5a5a5">
+//         <SearchForm value={movieName} onChange={updateQueryString} />
+//         {loading ? (
+//           <Skeleton
+//             count={15}
+//             style={{ height: 30, width: 300, marginTop: 15 }}
+//           />
+//         ) : searchResults.length === 0 && movieName ? (
+//           <h2> Nothing found</h2>
+//         ) : (
+//           <MoviesList films={searchResults} />
+//         )}
+//       </SkeletonTheme>
+//     </div>
+//   );
+// };
+
+// export default Movies;
