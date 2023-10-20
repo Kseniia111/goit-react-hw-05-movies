285873ccf209e780809d0a233d646f5e68f7aed8
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-20 12:38:20 +0300

    Update SearchForm.styled.js

--- src/components/SearchForm/SearchForm.styled.js
+++ src/components/SearchForm/SearchForm.styled.js
@@ -1,34 +1,12 @@
 import styled from 'styled-components';
 
-export const Form = styled.form`
-  display: flex;
-  align-items: center;
-  gap: 1.5rem;
-  padding: 0 16px;
-  flex-wrap: wrap;
-
-  input {
-    font-size: 1rem;
-    padding: 8px 32px 8px 8px;
-    width: 250px;
-    background-color: transparent;
-    border: none;
-    border-bottom: 2px solid #064e8a;
-    outline: none;
-  }
-`;
 export const InputSearch = styled.input`
   font-weight: 500;
-  font-size: 18px;
-  line-height: 27px;
-
-  padding: 15px 16px;
-
+  font-size: 16px;
+  padding: 12px;
   color: rgba(255, 255, 255, 0.2);
-
-  background: #2b2b2b;
-  border-radius: 10px;
-
+  background: #064e8a;
+  border-radius: 8px;
   :active,
   :focus,
   :hover {
@@ -36,23 +14,15 @@ export const InputSearch = styled.input`
   }
 `;
 export const ButtonSearch = styled.button`
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 27px;
-
-  margin-left: 12px;
-  padding: 15px 16px;
-
-  color: rgba(255, 255, 255, 0.2);
-
-  background: #2b2b2b;
-  border-radius: 10px;
-
-  :not(:disabled):active,
-  :focus,
-  :not(:disabled):hover {
-    color: white;
-  }
+  padding: 8px 16px;
+  margin: 16px;
+  border: none;
+  border-radius: 4px;
+  text-decoration: none;
+  color: white;
+  font-size: 16px;
+  background-color: #064e8a;
+  cursor: pointer;
 `;
 // export const Icon = styled(FcSearch)`
 //   width: 25px;
@@ -61,4 +31,40 @@ export const ButtonSearch = styled.button`
 //   right: 56px;
 //   z-index: 1;
 //   opacity: 0.7;
+// `;
+
+// font-weight: 500;
+// font-size: 18px;
+// line-height: 27px;
+
+// margin-left: 12px;
+// padding: 15px 16px;
+
+// color: rgba(255, 255, 255, 0.2);
+
+// background: #2b2b2b;
+// border-radius: 10px;
+
+// :not(:disabled):active,
+// :focus,
+// :not(:disabled):hover {
+//   color: white;
+// }
+
+// export const Form = styled.form`
+//   display: flex;
+//   align-items: center;
+//   gap: 1.5rem;
+//   padding: 0 16px;
+//   flex-wrap: wrap;
+
+//   input {
+//     font-size: 1rem;
+//     padding: 8px 32px 8px 8px;
+//     width: 250px;
+//     background-color: transparent;
+//     border: none;
+//     border-bottom: 2px solid #064e8a;
+//     outline: none;
+//   }
 // `;
